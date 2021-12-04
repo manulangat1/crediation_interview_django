319e3ae3a5a2b88bd7b2bae789daeb6005a1222f
manulangat1 <emmanuelthedeveloper.com>
2021-12-04 17:39:02 +0300

    rf-fix-no-application-error-aws

--- crediation/settings.py
+++ crediation/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['chekiWasee.eba-wch5ypcb.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
