e74be347c2d5b2a32dfb5333bc3b2eb10c66338b
manulangat1 <emmanuelthedeveloper.com>
2021-12-04 19:56:50 +0300

    rf-importing-os-module

--- crediation/settings.py
+++ crediation/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 """
     Initialise the environ package for loading env variables
 """
@@ -130,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
